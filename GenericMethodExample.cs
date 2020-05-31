adb9cb2f5b31644c3ef23fb16dfaec731997fdb9
Kedlawv <waldek.bus@gmail.com>
2020-06-01 00:42:57 +0200

    Generics example

---
+++ GenericMethodExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericMethodExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float floatValue = 13.5f;
+        Vector3 aVector3 = new Vector3(1, 2, 3);
+
+        Evaluate(floatValue);
+        Evaluate(aVector3);
+        Evaluate(5);
+        Evaluate("String");
+    }
+
+    private void Evaluate<T>(T suppliedValue)
+    {
+        Debug.LogFormat("the type of {0} is {1}", suppliedValue, typeof(T));
+    }
+}
