14fcc9f893a156235073c27bf1f212ac4b899d8e
Kedlawv <waldek.bus@gmail.com>
2020-05-31 22:59:07 +0200

    Initial | Sample Script |

---
+++ SampleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    public int numberToDisplay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SayHelloWorld();
+    }
+
+    private void SayHelloWorld()
+    {
+        for (int i = 0; i < numberToDisplay; i++){
+            Debug.Log("Hello World");
+        }
+    }
+
+    
+}
