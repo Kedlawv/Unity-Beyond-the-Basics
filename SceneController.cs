0df0b05a2f413150905f44ffbf2417b4830000f3
Kedlawv <waldek.bus@gmail.com>
2020-06-01 01:03:19 +0200

    Arrays

---
+++ SceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public string[] shapes = { "circle", "square", "triangle", "octagon" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(string s in shapes)
+        {
+            Debug.Log(s.ToUpper());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
