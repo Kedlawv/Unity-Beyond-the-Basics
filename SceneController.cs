528b445e5d145439304782162e9900fa966c37b0
Kedlawv <waldek.bus@gmail.com>
2020-06-01 01:22:33 +0200

    Arrays and Lists | List.Find(predicate) => lambda

--- SceneController.cs
+++ SceneController.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
+
     public string[] shapes = { "circle", "square", "triangle", "octagon" };
+    public List<string> moreShapes;
+
+    public List<Shape> gameShapes;
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach(string s in shapes)
+        Shape octagon = gameShapes.Find(s => s.Name == "Octagon");
+        octagon.SetColor(Color.red);
+
+        ShapesArray();
+        ShapesList();
+    }
+
+    private void ShapesArray()
+    {
+        foreach (string s in shapes)
         {
             Debug.Log(s.ToUpper());
+        }
+    }
+
+    private void ShapesList()
+    {
+        moreShapes = new List<string> { "circle", "square", "triangle", "octagon" };
+        moreShapes.Add("rectangle");
+        moreShapes.Insert(2, "diamond");
+
+        foreach (string s in moreShapes)
+        {
+            Debug.Log(s.ToLower());
         }
     }
 
