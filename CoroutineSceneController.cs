954b9064945f6b2ec48e78fa7d15ea284d417388
Kedlawv <waldek.bus@gmail.com>
2020-06-07 22:01:47 +0200

    Coroutine Example -> pauze execution

---
+++ CoroutineSceneController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineSceneController : MonoBehaviour
+{
+    public List<Shape> gameShapes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //SetShapesRed();
+            StartCoroutine(SetShapesBlue());
+        }
+        
+    }
+
+    private IEnumerator SetShapesBlue()
+    {
+        foreach (Shape shape in gameShapes)
+        {
+            shape.SetColor(Color.blue);
+            yield return new WaitForSeconds(1);
+            shape.SetColor(Color.white);
+        }
+
+        yield return new WaitForSeconds(1);
+        Debug.Log("I just wasted a second");
+    }
+
+    private void SetShapesRed()
+    {
+        foreach(Shape shape in gameShapes)
+        {
+            shape.SetColor(Color.red);
+        }
+    }
+}
