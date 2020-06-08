6b32d5af5cf641a21dafd273ec1b612fea7a3585
Kedlawv <waldek.bus@gmail.com>
2020-06-08 02:20:56 +0200

    GameSceneController | Player Controller | OOP principles in the context of Unity Engine

---
+++ PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Shape
+{
+    GameSceneController gameSceneController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSceneController = FindObjectOfType<GameSceneController>();
+        this.SetColor(Color.yellow);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(horizontalMovement) > Mathf.Epsilon)
+        {
+            horizontalMovement = horizontalMovement * Time.deltaTime * gameSceneController.playerSpeed;
+            horizontalMovement += this.transform.position.x;
+
+            this.transform.position = new Vector2(horizontalMovement, this.transform.position.y);
+        }
+    }
+}
